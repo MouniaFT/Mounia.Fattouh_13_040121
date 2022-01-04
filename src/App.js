2ea80c75f314959e9745c254deb29af7a3cfd970
mounia fattouh <mounia.fattouh@gmail.com>
2022-01-04 15:35:39 +0100

    Init project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+// import { Counter } from './features/counter/Counter'
+
+function App() {
+  return (
+    <div>
+      <header>
+        {/* <Counter /> */}
+        <h1>hello</h1>
+      </header>
+    </div>
+  )
+}
+
+export default App

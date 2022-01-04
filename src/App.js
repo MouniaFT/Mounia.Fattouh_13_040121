af253c7a35fe0512ce9e8b36fd3e0ea2a33541bf
mounia fattouh <mounia.fattouh@gmail.com>
2022-01-04 17:09:50 +0100

    Add footer component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 // import { Counter } from './features/counter/Counter'
 import Header from './components/Header'
+import Footer from './components/footer'
 
 function App() {
   return (
     <div>
       <Header />
       {/* <Counter /> */}
+      <Footer />
     </div>
   )
 }

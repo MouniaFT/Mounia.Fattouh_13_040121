6efd26635b5681ae0bb51ca312a0041c1436c8b8
mounia fattouh <mounia.fattouh@gmail.com>
2022-01-05 15:42:08 +0100

    Add Router

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 import React from 'react'
 // import { Counter } from './features/counter/Counter'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Home from './pages/Home'
 import Login from './pages/Login'
+import User from './pages/User'
 
 function App() {
   return (
-    <div>
+    <Router>
       <Header />
-      <Home />
-      <Login />
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route exact path="/login">
+        <Login />
+      </Route>
+      <Route exact path="/user">
+        <User />
+      </Route>
       {/* <Counter /> */}
       <Footer />
-    </div>
+    </Router>
   )
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,22 @@
 import logo from '../images/argentBankLogo.png'
+import { Link } from 'react-router-dom'
 
 export const Header = () => {
   return (
     <nav className="main-nav">
-      <a className="main-nav-logo" href="./index.html">
+      <Link to="/" className="main-nav-logo">
         <img
           className="main-nav-logo-image"
           src={logo}
           alt="Argent Bank Logo"
         />
         <h1 className="sr-only">Argent Bank</h1>
-      </a>
+      </Link>
       <div>
-        <a className="main-nav-item" href="./sign-in.html">
+        <Link to="/login" className="main-nav-item">
           <i className="fa fa-user-circle"></i>
           Sign In
-        </a>
+        </Link>
       </div>
     </nav>
   )

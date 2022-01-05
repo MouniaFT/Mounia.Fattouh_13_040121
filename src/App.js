9f88310f74f6b8b731ec1b8619d8a070c75efbfd
mounia fattouh <mounia.fattouh@gmail.com>
2022-01-05 15:16:50 +0100

    Add Login page

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import React from 'react'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Home from './pages/Home'
+import Login from './pages/Login'
 
 function App() {
   return (
     <div>
       <Header />
       <Home />
+      <Login />
       {/* <Counter /> */}
       <Footer />
     </div>

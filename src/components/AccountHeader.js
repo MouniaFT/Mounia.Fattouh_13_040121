29484d46a6e391c5c649c7d9a8809485a1ee1173
mounia fattouh <mounia.fattouh@gmail.com>
2022-01-05 21:27:01 +0100

    Add AccountHeader and AccountContent components

---
+++ src/components/AccountHeader.js
@@ -0,0 +1,14 @@
+export const AccountHeader = () => {
+  return (
+    <div className="header">
+      <h1>
+        Welcome back
+        <br />
+        Tony Jarvis!
+      </h1>
+      <button className="edit-button">Edit Name</button>
+    </div>
+  )
+}
+
+export default AccountHeader
--- src/pages/User.js
+++ src/pages/User.js
@@ -1,5 +1,39 @@
+import AccountContent from '../components/AccountContent'
+import AccountHeader from '../components/AccountHeader'
+
+const account = [
+  {
+    title: 'Argent Bank Checking (x8349)',
+    amount: '$2,082.79',
+    description: 'Available Balance',
+  },
+  {
+    title: 'Argent Bank Savings (x6712)',
+    amount: '$10,928.42',
+    description: 'Available Balance',
+  },
+  {
+    title: 'Argent Bank Credit Card (x8349)',
+    amount: '$184.30',
+    description: 'Current Balance',
+  },
+]
+
 export const User = () => {
-  return <h1>Hello</h1>
+  return (
+    <main className="main bg-dark">
+      <AccountHeader />
+      <h2 className="sr-only">Accounts</h2>
+      {account.map(({ title, amount, description }) => (
+        <AccountContent
+          key={title}
+          title={title}
+          amount={amount}
+          description={description}
+        />
+      ))}
+    </main>
+  )
 }
 
 export default User

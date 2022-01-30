5dd511ab7a545d413b1581a33c72c018072c37b9
mounia fattouh <mounia.fattouh@gmail.com>
2022-01-30 19:34:30 +0100

    Compile Sass

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react'
-// import { Counter } from './features/counter/Counter'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Header from './components/Header'
 import Footer from './components/Footer'
